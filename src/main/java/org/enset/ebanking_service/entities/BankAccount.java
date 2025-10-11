855ba43e33e24d5a7db6ccd6fdf0272b0aaa4ee2
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-11 22:45:10 +0100

    implement the controller and test with Postman

--- src/main/java/org/enset/ebanking_service/entities/BankAccount.java
+++ src/main/java/org/enset/ebanking_service/entities/BankAccount.java
@@ -1,6 +1,8 @@
 package org.enset.ebanking_service.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,7 +20,8 @@ public class BankAccount {
     @Id
     public String id;
     public Date createdAt;
-    public double balance;
+    public Double balance;
     public String currency;
+    @Enumerated(EnumType.STRING)
     private AccountType type;
 }
---
+++ src/main/java/org/enset/ebanking_service/web/AccountRestController.java
@@ -0,0 +1,46 @@
+package org.enset.ebanking_service.web;
+
+import org.enset.ebanking_service.entities.BankAccount;
+import org.enset.ebanking_service.repositories.BankAccountRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class AccountRestController {
+    private BankAccountRepository bankAccountRepository;
+
+    public AccountRestController(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    @GetMapping("/bankAccounts")
+    public List<BankAccount> bankAccounts(){
+        return bankAccountRepository.findAll();
+    }
+    @GetMapping("/bankAccounts/{id}")
+    public BankAccount bankAccount(@PathVariable String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("bank account not found",id)));
+    }
+    @PostMapping("/bankAccounts")
+    public BankAccount save (@RequestBody BankAccount bankAccount){
+        return bankAccountRepository.save(bankAccount);
+    }
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount){
+        BankAccount account = bankAccountRepository.findById(id).orElseThrow();
+        if (account.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+        if (account.getCreatedAt()!=null) account.setCreatedAt(new Date());
+        if (account.getType()!=null) account.setType(bankAccount.getType());
+        if(account.getCurrency()!=null) account.setCurrency(bankAccount.getCurrency());
+        return bankAccountRepository.save(account);
+    }
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount (@PathVariable String id){
+        bankAccountRepository.deleteById(id);
+
+    }
+
+}
