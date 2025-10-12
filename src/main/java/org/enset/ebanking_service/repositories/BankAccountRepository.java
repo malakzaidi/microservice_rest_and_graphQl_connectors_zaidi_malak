5cf4537ee020d4358f32fe1f71efc34bb071cd37
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-12 12:55:45 +0100

    implement a projection for Spring Data Rest

--- src/main/java/org/enset/ebanking_service/repositories/BankAccountRepository.java
+++ src/main/java/org/enset/ebanking_service/repositories/BankAccountRepository.java
@@ -1,11 +1,18 @@
 package org.enset.ebanking_service.repositories;
 
 import org.enset.ebanking_service.entities.BankAccount;
+import org.enset.ebanking_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 
 
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+    @RestResource(path="/byType")
+    List<BankAccount> findByType (@Param("t") AccountType type);
 }
